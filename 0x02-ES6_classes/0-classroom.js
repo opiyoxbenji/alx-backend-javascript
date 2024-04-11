00ae7e8dcca74b4f217025e50feafe2a531ac89c
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-11 11:09:59 +0000

    trial 1

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom.
+ */
+export default class classRoom {
+	/**
+	 *  Creates a new instance of ClassRoom.
+	 *  @param {number} maxStudentsSize - The maximum number of students
+	 *  that can be accommodated in the classroom.
+	 */
+	constructor(maxStudentSize) {
+		this._maxStudentSize = maxStudentSize;
+	}
+}
